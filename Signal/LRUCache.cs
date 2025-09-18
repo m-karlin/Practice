a5ddcddacfd57b4fdd165e7444c636615ac875c1
Mikhail Karlin <m.karlin@dodobrands.io>
2025-09-19 02:32:30 +0300

    update

---
+++ Signal/LRUCache.cs
@@ -0,0 +1,128 @@
+namespace Practice.Signal;
+
+public class LRUCache(int capacity)
+{
+	private class Node(int key, int value, Node? next, Node? prev)
+	{
+		public int Value { get; set; } = value;
+		public int Key { get; set; } = key;
+		public Node? Next { get; set; } = next;
+		public Node? Prev { get; set; } = prev;
+	}
+
+	private readonly Dictionary<int, Node> _dictionary = new(capacity);
+	private Node? _head;
+	private Node? _tail;
+	private int _count = 0;
+
+	public int Get(int key)
+	{
+		if (!_dictionary.TryGetValue(key, out var node)) return -1;
+		MoveToHead(node);
+		return node.Value;
+	}
+
+	public void Put(int key, int value)
+	{
+		if (_dictionary.TryGetValue(key, out var node))
+		{
+			node.Value = value;
+			MoveToHead(node);
+			return;
+		}
+
+		if (_count >= capacity)
+		{
+			RemoveTail();
+		}
+
+		var newNode = new Node(key, value, null, _head);
+		_dictionary.Add(key, newNode);
+		AddToHead(newNode);
+		_count++;
+	}
+
+	private void MoveToHead(Node node)
+	{
+		if (node == _head) return;
+		RemoveNode(node);
+		AddToHead(node);
+	}
+
+	private void RemoveNode(Node node)
+	{
+		if (node.Prev != null)
+			node.Prev.Next = node.Next;
+		if (node.Next != null)
+			node.Next.Prev = node.Prev;
+
+		if (node == _tail)
+			_tail = node.Prev;
+
+		if (node == _head)
+			_head = node.Next;
+
+		node.Prev = null;
+		node.Next = null;
+	}
+
+	private void AddToHead(Node node)
+	{
+		if (_head != null)
+		{
+			node.Next = _head;
+			_head.Prev = node;
+			_head = node;
+		}
+		else
+		{
+			_head = node;
+			_tail = node;
+		}
+	}
+
+	private void RemoveTail()
+	{
+		if (_tail == null) return;
+
+		_dictionary.Remove(_tail.Key);
+
+		if (_tail == _head)
+		{
+			_head = null;
+			_tail = null;
+		}
+		else
+		{
+			_tail = _tail.Prev;
+			_tail!.Next = null;
+		}
+
+		_count--;
+	}
+}
+
+public class LruCacheTests
+{
+	[Fact]
+	public void GetExistedValue()
+	{
+		var cache = new LRUCache(capacity: 2);
+		cache.Put(1, 1);
+		cache.Put(2, 2);
+		var result = cache.Get(1);
+		Assert.Equal(1, result);
+	}
+
+	[Fact]
+	public void TryGetEvictedValue()
+	{
+		var cache = new LRUCache(capacity: 2);
+		cache.Put(1, 1);
+		cache.Put(2, 2);
+		cache.Get(1);
+		cache.Put(3, 3);
+		var result = cache.Get(2);
+		Assert.Equal(-1, result);
+	}
+}
---
+++ Signal/NodesCleaner.cs
@@ -0,0 +1,180 @@
+using Xunit.Abstractions;
+
+namespace Practice.Signal;
+
+/*
+    The task is to remove from the object tree all nodes
+    where the Alive property is false. However, if a node
+    has an Alive property that is true, all of its parents
+    and children should remain in the tree.
+
+    Extra:
+    Perform the task using recursive and iterative algorithms,
+    explain in which cases each one should be used, as well as
+    the advantages and disadvantages of each approach.
+*/
+
+public record Node(int Id, int? ParentId, bool Alive, List<Node> Children)
+{
+	public Node CreateChild(bool alive)
+	{
+		var child = new Node(
+			(Children.Count > 0 ? Children[^1].Id : Id * 10) + 1,
+			Id,
+			alive,
+			[]
+		);
+		Children.Add(child);
+		return child;
+	}
+
+	public static void Clean(Node node)
+	{
+	}
+
+	public static string Stringify(Node node, int level = 0)
+	{
+		return $"{new string('-', level)}{node.Alive.ToString()[0]}{node.Id}"
+		       + Environment.NewLine
+		       + string.Join("", node.Children.Select(child => Stringify(child, level + 1)));
+		/*return $"{new string(' ', level * 2)}Id:{node.Id} pId{(node.ParentId.HasValue ? node.ParentId : "R")}:{node.Alive}"
+		    + Environment.NewLine
+		    + string.Join("", node.Children.Select(child => Stringify(child, level + 1)));*/
+	}
+};
+
+public class NodesCleanerTests(ITestOutputHelper output)
+{
+	[Fact]
+	public void Test()
+	{
+		var root = new Node(1, null, true, []);
+
+		var l11 = root.CreateChild(false);
+		var l12 = root.CreateChild(true);
+
+		l11.CreateChild(false).CreateChild(true);
+		l12.CreateChild(false).CreateChild(true);
+
+		root.CreateChild(true).CreateChild(false);
+		root.CreateChild(false).CreateChild(true);
+
+		//visualize tree
+		Print(root);
+
+		Node.Clean(root);
+
+		Assert.Equal(
+			Node.Stringify(root).Trim(),
+			"""
+				T1
+				-F11
+				--F111
+				---T1111
+				-T12
+				--F121
+				---T1211
+				-T13
+				--F131
+				-F14
+				--T141
+				""".Trim()
+		);
+	}
+
+	[Fact]
+	public void Test2()
+	{
+		var root = new Node(1, null, false, []);
+		root.CreateChild(false).CreateChild(false).CreateChild(false);
+		root.CreateChild(false).CreateChild(true).CreateChild(false);
+		//visualize tree
+		//Print(root);
+		Node.Clean(root);
+		Assert.Equal(
+			Node.Stringify(root).Trim(),
+			"""
+				F1
+				-F12
+				--T121
+				---F1211
+				""".Trim()
+		);
+	}
+
+	[Fact]
+	public void Test3()
+	{
+		var root = new Node(1, null, false, []);
+		root.CreateChild(false).CreateChild(false).CreateChild(true);
+		root.CreateChild(false).CreateChild(true).CreateChild(true);
+		root.CreateChild(true).CreateChild(true).CreateChild(true);
+		root.CreateChild(false).CreateChild(true).CreateChild(true);
+		root.CreateChild(false).CreateChild(false).CreateChild(true);
+		root.CreateChild(false).CreateChild(false).CreateChild(false);
+		var l11 = root.CreateChild(false);
+		l11.CreateChild(false);
+		l11.CreateChild(true);
+		l11.CreateChild(false).CreateChild(false);
+		l11.CreateChild(false).CreateChild(true);
+		l11.CreateChild(true).CreateChild(false);
+		l11.CreateChild(true).CreateChild(true);
+
+		var l12 = root.CreateChild(true);
+		l12.CreateChild(false);
+		l12.CreateChild(true);
+		l12.CreateChild(false).CreateChild(false);
+		l12.CreateChild(false).CreateChild(true);
+		l12.CreateChild(true).CreateChild(false);
+		l12.CreateChild(true).CreateChild(true);
+
+		//visualize tree
+		//Print(root);
+		Node.Clean(root);
+		Assert.Equal(
+			Node.Stringify(root).Trim(),
+			"""
+				F1
+				-F11
+				--F111
+				---T1111
+				-F12
+				--T121
+				---T1211
+				-T13
+				--T131
+				---T1311
+				-F14
+				--T141
+				---T1411
+				-F15
+				--F151
+				---T1511
+				-F17
+				--T172
+				--F174
+				---T1741
+				--T175
+				---F1751
+				--T176
+				---T1761
+				-T18
+				--F181
+				--T182
+				--F183
+				---F1831
+				--F184
+				---T1841
+				--T185
+				---F1851
+				--T186
+				---T1861
+				""".Trim()
+		);
+	}
+
+	private void Print(Node node)
+	{
+		output.WriteLine(Node.Stringify(node));
+	}
+}
