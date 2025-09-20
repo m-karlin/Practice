a18a959434691eabc60459a3ed2e610ce263a311
Mikhail Karlin <m.karlin@dodobrands.io>
2025-09-20 03:11:45 +0300

    update

---
+++ Problems/ReverseString.cs
@@ -0,0 +1,25 @@
+﻿namespace Practice.Problems;
+
+// https://solvit.space/coding/2873
+public class ReverseStringTests
+{
+	private static void ReverseString(string[] s)
+	{
+		var i = 0;
+		var j = s.Length - 1;
+		while (i <= j)
+		{
+			(s[i], s[j]) = (s[j], s[i]);
+			i++;
+			j--;
+		}
+	}
+
+	[Fact]
+	public void Test()
+	{
+		string[] s = ["H", "a", "n", "n", "a", "h"];
+		ReverseString(s);
+		Assert.Equal(["h","a","n","n","a","H"], s);
+	}
+}
