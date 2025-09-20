243a5677287a97b1aaade2e9291d64c320431f6b
Mikhail Karlin <m.karlin@dodobrands.io>
2025-09-20 03:14:00 +0300

    update

--- Problems/ReverseString.cs
+++ Problems/ReverseString.cs
@@ -9,7 +9,9 @@ public class ReverseStringTests
 		var j = s.Length - 1;
 		while (i <= j)
 		{
-			(s[i], s[j]) = (s[j], s[i]);
+			string t = s[j];
+			s[j] = s[i];
+			s[i] = t;
 			i++;
 			j--;
 		}
