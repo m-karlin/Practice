21de120b3ffff9fcb7cc76fe12e6a10a9bb2d472
Mikhail Karlin <m.karlin@dodobrands.io>
2025-09-23 03:08:05 +0300

    update

---
+++ App/NetFeatures/As.cs
@@ -0,0 +1,17 @@
+﻿namespace App.NetFeatures;
+
+public class As
+{
+	public static void Run()
+	{
+		object obj = "Hello World";
+
+		// Опасное приведение - может вызвать исключение
+		// string str1 = (string)obj; // Работает
+		// int number = (int)obj;     // InvalidCastException
+
+		// Безопасное приведение с 'as'
+		string str2 = obj as string; // Успешно
+		int? number = obj as int?;   // Вернет null (для значимых типов используйте nullable)
+	}
+}
