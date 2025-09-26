8dc93110b2149ffe300627a4c588520298b954fc
Mikhail Karlin <m.karlin@dodobrands.io>
2025-09-26 03:29:25 +0300

    update

---
+++ App.Tests/MissingNumberTests.cs
@@ -0,0 +1,20 @@
+﻿using App.Problems;
+
+namespace App.Tests;
+
+public class MissingNumberTests
+{
+	[Fact]
+	public void Test1()
+	{
+		var result = MissingNumber.Find([3, 0, 1]);
+		Assert.Equal(2, result);
+	}
+
+	[Fact]
+	public void Test2()
+	{
+		var result = MissingNumber.Find([0, 1]);
+		Assert.Equal(2, result);
+	}
+}
---
+++ App/Problems/MissingNumber.cs
@@ -0,0 +1,15 @@
+﻿namespace App.Problems;
+
+// Дан массив nums, содержащий n различных чисел в диапазоне [0, n]. 
+// Необходимо вернуть единственное число из диапазона, которого не хватает в массиве;
+
+public static class MissingNumber
+{
+	public static int Find(int[] nums)
+	{
+		var n = nums.Length;
+		var expectedSum = n * (n + 1) / 2;
+		var actualSum = nums.Sum();
+		return expectedSum - actualSum;
+	}
+}
